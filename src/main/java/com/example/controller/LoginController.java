b7048c6f8482b5c56fe26cf0f9228d2209d1486d
kim gaeun <31676033+rlarkdms@users.noreply.github.com>
2021-06-03 18:15:37 +0900

    로그인 해결

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,7 @@ import com.example.data.Member;
 
 @Controller
 public class LoginController {
-	
+	@Autowired
 	private MemberDao memberDao;
 	
 
@@ -28,23 +29,30 @@ public class LoginController {
 	
 	@PostMapping("/login/loginCheck")
 	public String hello(
-			@RequestParam(value="status",required=false) String status,
+			@RequestParam(value="status",required=true) String status,
 			@RequestParam(value = "member_id", required = true) String id,
 			@RequestParam(value = "password", required = true) String pwd,Model model) {
 		
 		System.out.print(status+id+pwd);
 		
-		if(status=="normal") {
+		if(status.equals("normal")) {
+			System.out.print("들어갔는지 확인하기");
 			String memberlist= memberDao.ConfirmIDPWD(id, pwd);
+			
+			System.out.print(memberlist);
 				if (memberlist.equals(null)) {
 						model.addAttribute("error","아이디와 비밀번호가 맞지 않습니다.");
 						return "redirect:/login/login";
 			
 				}
 				else {
+					
+					
 					model.addAttribute("id", memberlist);
+					return "/login/loginCheck";
 				}
 		}else {
+			System.out.print("else문임");
 			//관리자 나중에 만들기.
 			
 		}
@@ -52,4 +60,8 @@ public class LoginController {
 		return "/login/loginCheck";
 	}
 
+	@GetMapping("/index")
+	public String index() {
+		return "/index";
+	}
 }
