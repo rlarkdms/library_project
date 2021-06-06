bc17319f3ce09fa81ac37235f608ffb3137e398d
kim gaeun <31676033+rlarkdms@users.noreply.github.com>
2021-06-06 21:37:14 +0900

    book_id 추가

--- src/main/java/com/example/dao/MyPageDao.java
+++ src/main/java/com/example/dao/MyPageDao.java
@@ -15,10 +15,10 @@ public class MyPageDao {
 	this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 	public List<Return_List> selectReturnAll(String member_id) {//전부다 고르는거
-	List<Return_List> results = jdbcTemplate.query(("select book.book_name,borrow_list.book_return_date,extend_confirm from borrow_list Inner join book on borrow_list.book_id=book.book_id where borrow_list.member_id='"+member_id+"';"),
+	List<Return_List> results = jdbcTemplate.query(("select book.book_id,book.book_name,borrow_list.book_return_date,extend_confirm from borrow_list Inner join book on borrow_list.book_id=book.book_id where borrow_list.member_id='"+member_id+"';"),
 	(ResultSet rs, int rowNum) -> {
-		Return_List turn=new Return_List(rs.getString("Book_name"), rs.getString("book_return_date"), rs.getLong("extend_confirm"));
-		turn.setBook_name(rs.getString("Book_name"));
+		Return_List turn=new Return_List(rs.getLong("book_id"),rs.getString("book_name"), rs.getString("book_return_date"), rs.getLong("extend_confirm"));
+		turn.setBook_id(rs.getLong("book_id"));
 	
 	return turn;
 
--- src/main/java/com/example/data/Return_List.java
+++ src/main/java/com/example/data/Return_List.java
@@ -4,16 +4,29 @@ import java.sql.Date;
 
 public class Return_List {
 
+	private Long book_id;
 	private String Book_name;
 	private String return_date;
 	private Long extend_confirm;
 	
-	public Return_List(String Book_name,String return_date,Long extend_confirm) {
+	public Return_List(Long book_id,String Book_name,String return_date,Long extend_confirm) {
+		this.book_id=book_id;
 		this.Book_name=Book_name;
 		this.return_date=return_date;
 		this.extend_confirm=extend_confirm;
 		
 	}
+	
+
+	public Long getBook_id() {
+		return book_id;
+	}
+
+
+	public void setBook_id(Long book_id) {
+		this.book_id = book_id;
+	}
+
 
 	public String getBook_name() {
 		return Book_name;
